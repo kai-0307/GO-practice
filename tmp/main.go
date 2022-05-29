8931dd39037930ffc8c77512020d597af691b8a3
kai-0307 <93191804+kai-0307@users.noreply.github.com>
2022-05-30 00:32:15 +0900

    main

--- tmp/main.go
+++ tmp/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("hello,docker!")
+
+	// Printf / Sqrt はそれぞれのパッケージからエクスポートされている
+	fmt.Printf("Sqrt(9) is %g\n", math.Sqrt(9))
+
+	// 小文字で始まる名前は外部に公開されないので、エラーが発生する
+	fmt.Println(math.Pi)
+}
+
+//関数
+
+//関数はfuncで定義する。
+//引数の型は変数の後ろに書く。
+
+// ２つのintを受け取り、intを返す関数
+
+func add(x int, y int) int {
+	return x + y 
 }
